aeca13fc8e4f663b1a7e3794c0ffc8959ad3cc8b
Hillorius <jiwabisabernafas@gmail.com>
2024-12-16 11:28:18 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
--- ServerScript (Place in ServerScriptService)
-local Players = game:GetService("Players")
 
 -- ASCII Clown
 local clownASCII = [[
@@ -22,14 +20,11 @@ local clownASCII = [[
 
 -- Function to handle clowning the player
 local function clownAndKick(player)
-    -- Prompt the player to press F9 in their client chat
-    player:Kick("dummy, got trolled, --Hillorius")
     print(clownASCII)
 end
 
 -- Connect to PlayerAdded
 Players.PlayerAdded:Connect(function(player)
-    -- Slight delay to ensure the player fully loads
     wait(1)
     clownAndKick(player)
 end)
